f0b20f67d095e71232e477b8ca83fd96075a58f8
ZachGerhart <zgerhart@gmail.com>
2017-10-10 17:27:01 -0500

    modified

--- callbackPractice.js
+++ callbackPractice.js
@@ -1,14 +1,14 @@
 /* In this repo your job is to write functions to make each function call work properly.
-Below is a sample problem 
+Below is a sample problem
 
   //code here for sayHi
 
    sayHi('Hi Katie', function(thingToSay){
       alert(thingToSay);
    });
-   
-and what you should write is the sayHi function that makes the code above work, 
-    
+
+and what you should write is the sayHi function that makes the code above work,
+
    var sayHi = function(str, cb){
     cb(str);
    }
@@ -16,15 +16,17 @@ and what you should write is the sayHi function that makes the code above work,
    sayHi('Hi Katie', function(thingToSay){
       alert(thingToSay); //should alert ('Hi Katie')'
    });
-    
+
 */
 
 
 // 1. Write a function called first that returns the first item of the array using a callback function
-
+function first (names, cb) {
+  cb(names[0])
+}
   // Code Here
 
-  
+
 var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
 first(names, function(firstName){
   console.log('The first name in names is ' + firstName);
@@ -34,7 +36,9 @@ first(names, function(firstName){
 
 
 // 2. Write a function called last which returns the last item of the array using a callback function.
-
+function last(names,cb){
+  cb(names[names.length - 1]);
+}
   //Code Here
 
 
@@ -46,8 +50,11 @@ last(names, function(lastName){
 
 
 
-// 3. Write a function called multiply that multiplies two numbers. Invoke the callback with the result of the multiplication. 
-
+// 3. Write a function called multiply that multiplies two numbers. Invoke the callback with the result of the multiplication.
+function multiply (num1, num2, cb) {
+  var product = num1 * num2
+  cb(product);
+}
   //Code Here
 
 
@@ -58,12 +65,20 @@ multiply(4, 3, function(answer){
 
 
 
-// 4. Write a function called contains that checks if a name exists in an array. 
-// If it does, invoke the callback with true as an argument. 
+// 4. Write a function called contains that checks if a name exists in an array.
+// If it does, invoke the callback with true as an argument.
 // If the name does not exist, invoke the callback with false as an argument.
 
-  //Code Here 
-
+  //Code Here
+function contains (arr, str, cb) {
+  for (var i = 0; i < arr.length; i++){
+    if(arr[i] === str){
+      cb(true);
+    } else {
+      cb(false)
+    }
+  }
+}
 
 
 
@@ -80,7 +95,15 @@ contains(names, 'Colt', function(result){
 // 5. Write a function called uniq that takes the names array and removes all duplicates.
 // Invoke the callback with the modified array as an argument.
 
-  //Code Here
+function uniq (array, cb) {
+    var empty = [];
+  for (var i = 0; i < array.length; i++ ){
+    if (empty.indexOf(array[i]) === -1){
+        empty.push(array[i]);
+      }
+    }
+    cb(empty);
+}//Code Here
 
 
 
@@ -91,8 +114,12 @@ uniq(names, function(uniqArr){
 
 // 6. Write a function called each that takes in an array of names. For each name in the array, invoke the callback and pass in the name and the name's index as arguments.
 
-    //Code Here 
-
+    //Code Here
+function each (arr, cb) {
+  for (var i = 0; i < arr.length; i++){
+    cb(names[i], i)
+  }
+}
 
 
 each(names, function(item, indice){
@@ -105,7 +132,13 @@ each(names, function(item, indice){
 // When the correct user object is found, invoke the callback with the user object as an argument.
 
 // Code here
-
+function getUserById (arr, str, cb) {
+ for(var i = 0; i < arr.length; i++){
+   if(arr[i].id === str){
+     cb(arr[i])
+   }
+ }
+}
 
 
 var users = [
@@ -130,5 +163,5 @@ var users = [
 ];
 
 getUserById(users, '16t', function(user){
-  console.log('The user with the id 16t has the email of ' + user.email + ' the name of ' + user.name + ' and the address of ' + user.address); 
+  console.log('The user with the id 16t has the email of ' + user.email + ' the name of ' + user.name + ' and the address of ' + user.address);
 });
